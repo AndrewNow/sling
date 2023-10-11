6428715357018f2b4f5371bb00f330408d9066d3
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2023-10-11 13:03:26 -0400

    pushing

--- src/lib/api.js
+++ src/lib/api.js
@@ -4,6 +4,9 @@ export async function getQuotePost() {
   const query = `*[_type == "quote"] {
     title,
     "slug": slug.current, 
+    preparedOn,
+    visible,
+    "clientLogo": clientLogo.asset->url,
     body
   }`;
   const data = await sanityClient.fetch(query);
