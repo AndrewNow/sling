e70f0257811d3036ab382546101ff565ac9d49ce
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2023-10-11 20:47:28 -0400

    pushing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import sanity from "@sanity/astro";
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   integrations: [
     sanity({
       projectId: "m2uqkqq4",
