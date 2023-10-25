6042d2f653a961b15d125438c9f28a6d52857732
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2023-10-25 16:05:18 -0400

    pushing shader

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 import sanity from "@sanity/astro";
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
-
+import glsl from 'vite-plugin-glsl';
+// import glslify from 'vite-plugin-glslify'
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
+  vite: {
+    plugins: [
+      glsl(),
+      // glslify()
+    ],
+  },
   integrations: [
     sanity({
       projectId: "m2uqkqq4",
