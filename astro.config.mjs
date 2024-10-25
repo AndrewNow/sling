4b15b0de5dedf8ace0b3768472d3bd83a744bc52
Andrew Nowacki <andrew.p.nowacki@gmail.com>
2024-10-25 10:14:40 -0400

    uploading

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,23 +3,23 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 import glsl from 'vite-plugin-glsl';
 // import glslify from 'vite-plugin-glslify'
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
   vite: {
-    plugins: [
-      glsl(),
-      // glslify()
-    ],
+    plugins: [glsl()
+    // glslify()
+    ]
   },
-  integrations: [
-    sanity({
-      projectId: "m2uqkqq4",
-      dataset: "production",
-      apiVersion: "2023-10-10",
-      // Set useCdn to false if you're building statically.
-      useCdn: false,
-    }),
-  ],
+
+  integrations: [sanity({
+    projectId: "m2uqkqq4",
+    dataset: "production",
+    apiVersion: "2023-10-10",
+    // Set useCdn to false if you're building statically.
+    useCdn: false
+  }), tailwind()]
 });
